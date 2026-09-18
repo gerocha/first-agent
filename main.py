ec3305afa0a31bdb113ae7d1e5b831a0e7f12d0f
Gelso Junior <gelso.rocha@pinpag.com.br>
2026-09-17 23:30:04 -0300

    add prompt

--- main.py
+++ main.py
@@ -3,14 +3,55 @@ from huggingface_hub import InferenceClient
 
 HF_TOKEN = os.environ.get("HF_TOKEN")
 
+SYSTEM_PROMPT = """Answer the following questions as best you can. You have
+access to the following tools:
+    get_weather: Get the current weather in a given location
+
+    The way you use the tools is by specifying a json blob.
+    Specially, this json shoud have an `action` key (with the name of the tool
+    to use) and an `action_input` key (with the input to the tool going here).
+
+    The only values that should be in the "action" field are:
+    get_weather: Get the current weather in a given location, args:
+    {"location": {"type": "string"}}
+    example use:
+
+    {{
+        "action": "get_weather",
+        "action_input": {{"location": "New York"}}
+    }}
+
+
+    ALWAYS use the following format:
+
+    Question: the input question you must answer
+    Thought: you should always think about one action to take. Only one action
+    at a time in this format:
+    Action:
+
+    $JSON_BLOB (inside markdown cell)
+
+    Observation: the result of the action. This Observation is unique,
+    complete, and the source of truth.
+    ... (this Thought/Action/Observation can repeat N times, you should take several steps when needed. The $JSON_BLOB must be formatted as markdown and only use a SINGLE action at a time.)
+
+    You must always output with the following format:
+
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+
+    Now begin! Reminder to ALWAYS use the exact characters `Final Answer:` when
+    you provide the definitive answer. """
+
 client = InferenceClient(model="moonshotai/Kimi-K2.5", token=HF_TOKEN)
 
 output = client.chat.completions.create(
         messages=[
-            {"role": "user", "content": "The capital of France is"},
+            {"role": "system", "content": SYSTEM_PROMPT},
+            {"role": "user", "content": "What's the weather in Praia Grande?"},
             ],
         stream=False,
-        max_tokens=1024,
+        max_tokens=150,
         extra_body={"thinking": {"type": "disabled"}},
         )
 
