6015555fa68d81650e67cdac44fd179af91ef8e3
Gelso Junior <gelso.rocha@pinpag.com.br>
2026-09-17 23:08:01 -0300

    testing completion

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from first-agent!")
+import os
+from huggingface_hub import InferenceClient
 
+HF_TOKEN = os.environ.get("HF_TOKEN")
 
-if __name__ == "__main__":
-    main()
+client = InferenceClient(model="moonshotai/Kimi-K2.5", token=HF_TOKEN)
+
+output = client.chat.completions.create(
+        messages=[
+            {"role": "user", "content": "The capital of France is"},
+            ],
+        stream=False,
+        max_tokens=1024,
+        extra_body={"thinking": {"type": "disabled"}},
+        )
+
+print(output.choices[0].message.content)
